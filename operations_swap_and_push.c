322cc4348a8cb4ca6d8fead3d65c820a12540553
Tomas Bruha <tomasbruha1@gmail.com>
2024-10-11 15:14:11 +0200

    update on operations + working linked list

--- operations_swap_and_push.c
+++ operations_swap_and_push.c
@@ -16,7 +16,27 @@ void	ft_sa(t_stack **stack_a, int print)
 {
 	t_stack	*temp;
 
-	temp = ;
+	if (!*stack_a || ft_lstsize(*stack_a) < 2)
+		return ;
+	temp = *stack_a;
+	(*stack_a) = (*stack_a)->next;
+	temp->next = (*stack_a)->next;
+	(*stack_a)->next = temp;
 	if (print == 1)
 		write(1, "sa\n", 3);
 }
+
+void	ft_sb(t_stack **stack_b, int print)
+{
+	t_stack	*temp;
+
+	if (!*stack_b || !(*stack_b)->next)
+		return ;
+	temp = (*stack_b);
+	(*stack_b) = (*stack_b)->next;
+	temp->next = (*stack_b)->next;
+	(*stack_b)->next = temp;
+	if (print == 1)
+		write(1, "sb\n", 3);
+}
+
